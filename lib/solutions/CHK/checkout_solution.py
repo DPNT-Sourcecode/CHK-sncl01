663bbd0b4f311ca070cf5716d4a6add14a456d35
sbx_user1051 <sbx_user1051@169.254.23.193>
2024-04-10 15:55:50 +0000

    Wed Apr 10 15:55:50 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,9 +1,13 @@
-# noinspection PyUnusedLocal
-# skus = unicode string
+import re
+
 ITEMS = {"A": 50, "B": 30, "C": 20, "D": 15}
 
 
+# noinspection PyUnusedLocal
+# skus = unicode string
 def checkout(skus):
-    for char in skus:
-        print(char)
+    letters = re.findall(r"\w", skus)
+
+    print(letters)
+
 
