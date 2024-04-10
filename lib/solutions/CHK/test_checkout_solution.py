599c38770141b600892ea49d5b702dba28b36d82
sbx_user1051 <sbx_user1051@169.254.23.193>
2024-04-10 15:49:50 +0000

    Wed Apr 10 15:49:50 UTC 2024

---
+++ lib/solutions/CHK/test_checkout_solution.py
@@ -0,0 +1,13 @@
+from .checkout_solution import checkout
+
+
+def test_checkout_solution():
+    assert checkout("NOT AN ITEM") == -1
+
+    assert checkout("A") == 50
+    assert checkout("B") == 30
+    assert checkout("C") == 20
+    assert checkout("D") == 15
+
+    assert checkout("AAA") == 130
+    assert checkout("BB") == 45
